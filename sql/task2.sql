6704349cc8ddcbd3ddc92bf5c071c83ed2d9657f
ZoobiaS <ZoobiaS@users.noreply.github.com>
2024-01-28 16:56:23 -0500

    Update task2.sql

--- sql/task2.sql
+++ sql/task2.sql
@@ -3,10 +3,21 @@
 -- The result should include the product ID, product name, and the average rating.
 -- Hint: You may need to use subqueries or common table expressions (CTEs) to solve this problem.
 
+-- this script will retrieve the top 100 products based on the average rating.
+select top 100 r.product_id, p.product_name, avg(rating) from Reviews r
+  -- join the reviews table with the products table to get the rating as well as product name 
+join Products p on p.product_id = r.product_id 
+  -- group by the product ID and name to get the average rating for each product 
+group by r.product_id, p.product_name 
+  -- order by the average rating to retrieve only the top ones 
+order by avg(rating) desc
+
 -- Problem 6: Retrieve the users who have made at least one order in each category
 -- Write an SQL query to retrieve the users who have made at least one order in each category.
 -- The result should include the user ID and username.
 -- Hint: You may need to use subqueries or joins to solve this problem.
+
+
 
 -- Problem 7: Retrieve the products that have not received any reviews
 -- Write an SQL query to retrieve the products that have not received any reviews.
